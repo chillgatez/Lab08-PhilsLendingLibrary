bfdfa988444bb90dab8c533ee2b40332bd329dcb
Kelsee Watts <watts.kelsee@gmail.com>
2023-07-31 11:26:53 -0500

    getting started

---
+++ Lab08-PhilsLendingLibrary/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab08_PhilsLendingLibrary
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int NumberOfPages { get; set; }
+        public Book(string title, string author, int numberOfPages)
+        {
+            Title = title;
+            Author = author;
+            NumberOfPages = numberOfPages;
+        }
+    }
+}
---
+++ Lab08-PhilsLendingLibrary/Library.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab08_PhilsLendingLibrary
+{
+    internal class Library : ILibrary
+    {
+        private Dictionary<string, Book> Storage;
+
+
+        public Library()
+        {
+            Storage = new Dictionary<string, Book>();
+        }
+
+        public int Count => throw new NotImplementedException();
+
+        public void Add(string title, string firstName, string lastName, int numberOfPages)
+        {
+
+        }
+
+        public Book Borrow(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Return(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book Search(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
