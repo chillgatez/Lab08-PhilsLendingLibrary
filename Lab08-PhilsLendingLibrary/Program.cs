e4863e0521aa5a1c8d0a4ac3183b6c0b664c8367
Kelsee Watts <watts.kelsee@gmail.com>
2023-07-31 10:44:34 -0500

    Add project files.

---
+++ Lab08-PhilsLendingLibrary/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab08_PhilsLendingLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
