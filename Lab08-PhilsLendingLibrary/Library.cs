2e7d2a36a1464733b70f0905a16ae17171f4089b
Kelsee Watts <watts.kelsee@gmail.com>
2023-07-31 14:21:45 -0500

    backpack class implemented

--- Lab08-PhilsLendingLibrary/Library.cs
+++ Lab08-PhilsLendingLibrary/Library.cs
@@ -11,42 +11,59 @@ namespace Lab08_PhilsLendingLibrary
     {
         private Dictionary<string, Book> Storage;
 
-
         public Library()
         {
             Storage = new Dictionary<string, Book>();
+
+            // Create a new Book object
+            Book book1 = new Book("To Kill A MockingBird", "SomeGuy", 100);
+            Book book2 = new Book("Americana", "Shemamonda", 100);
+            Book book3 = new Book("Berserk", "Kentaro Miura", 100);
+
+            // Add the book to the Storage dictionary using the title as the key
+            Storage.Add(book1.Title, book1);
+            Storage.Add(book2.Title, book2);
+            Storage.Add(book3.Title, book3);
         }
 
-        public int Count => throw new NotImplementedException();
+        public int Count => Storage.Count;
 
-        public void Add(string title, string firstName, string lastName, int numberOfPages)
+        public void Add(string title, string author, int numberOfPages)
         {
-
         }
 
         public Book Borrow(string title)
         {
-            throw new NotImplementedException();
+            // Search for the book in the Storage dictionary using the title as the key
+            // If found, return the book, otherwise return null
+            if (Storage.TryGetValue(title, out Book book))
+            {
+                Storage.Remove(title); // Remove the book from the dictionary
+                return book;
+            }
+            return null;
         }
 
-        public IEnumerator<Book> GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+        public IEnumerator<Book> GetEnumerator() => Storage.Values.GetEnumerator();
 
         public void Return(Book book)
         {
-            throw new NotImplementedException();
+            // Add the returned book back to the Storage dictionary
+            Storage.Add(book.Title, book);
+
         }
 
         public Book Search(string title)
         {
-            throw new NotImplementedException();
+            // Search for the book in the Storage dictionary using the title as the key
+            // If found, return the book, otherwise return null
+            if (Storage.TryGetValue(title, out Book book))
+            {
+                return book;
+            }
+            return null;
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
